3cf80c1f275eb245176b9e9db4d73ed9f8c54d8e
Belomir <ds.alex@tut.by>
2025-03-05 22:01:24 +0300

    Almost all is working but when i add new 6 cards they render only when i minimized browser

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -13,26 +13,37 @@ export const Main = ({ isSearch, searchText }: ISearchValue) => {
   const [start, setStartVal] = useState<number>(1);
   const [limit, setlimitVal] = useState<number>(6);
   const { data: cryptoData, isError, isLoading } = useCryptoData(start, limit);
+  const [height, setHeight] = useState(0);
 
-  console.log(cryptoData?.data.data, "data");
-  console.log("is - ", isSearch, " val - ", searchText);
+  //console.log(cryptoData?.data.data, "data");
+  //console.log("is - ", isSearch, " val - ", searchText);
 
-  const filteredCryptoData = useMemo(
-    () =>
-      isSearch
-        ? cryptoData?.data.data.filter(
-            (item: crptItm) =>
-              item.name.includes(searchText) || item.nameid.includes(searchText)
-          )
-        : cryptoData?.data.data,
-    []
-  );
+  const filteredCryptoData = useMemo(() => {
+    console.log(cryptoData?.data.data);
+    return isSearch
+      ? cryptoData?.data.data.filter(
+          (item: crptItm) =>
+            item.name.includes(searchText) || item.nameid.includes(searchText)
+        )
+      : cryptoData?.data.data;
+  }, [cryptoData, limit, searchText]);
 
   const handleOnButtonClick = () => {
-    console.log(123);
-    //const { data: cryptoData, isError, isLoading } = useCryptoData(1, 12);
+    setlimitVal(limit + 6);
+    setHeight(height + 80);
+    document.querySelector(".container");
   };
+  useEffect(() => {
+    if (!filteredCryptoData) {
+      setHeight(0);
+    } else if (filteredCryptoData.length < 6) setHeight(80);
+    else {
+      console.log(height, "1asdasd");
+      setHeight((parseInt(filteredCryptoData.length) / 6) * 80);
+    }
+  }, [filteredCryptoData?.length]);
 
+  console.log(height);
   if (isError) {
     return <h1>Error</h1>;
   }
@@ -42,11 +53,17 @@ export const Main = ({ isSearch, searchText }: ISearchValue) => {
   }
 
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={{
+        height: `${height}%`,
+      }}
+    >
       <div className="crptCont">
         {filteredCryptoData?.map((item: crptItm) => {
           return (
             <CryptoCard
+              key={item.id}
               id={item.id}
               name={item.name}
               tsupply={item.tsupply}
@@ -58,10 +75,14 @@ export const Main = ({ isSearch, searchText }: ISearchValue) => {
           );
         })}
       </div>
-      <div className="add6words" onClick={handleOnButtonClick}>
-        {" "}
-        add six more words
-      </div>
+      {!isSearch ? (
+        <div className="add6words" onClick={handleOnButtonClick}>
+          {" "}
+          add six more words
+        </div>
+      ) : (
+        <h2>This is what we find</h2>
+      )}
     </div>
   );
 };
