db5906896836760b571ca5b3a07ca5182243372e
Belomir <ds.alex@tut.by>
2025-03-25 17:23:18 +0300

    create some new features and now you can buy cripta and top up your wallet

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { ProfilePage } from "./pages/index.ts";
+import { TopUpPage } from "./pages/index.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/page2" element={<ProfilePage />} />
+        <Route path="/topUPpage" element={<TopUpPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
---
+++ src/pages/topUpPage/topUpPage.element.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./topUpPage.style.css";
+import { ExitToMainMenu } from "../../components";
+
+export const TopUpPage = () => {
+  const [money, setMoney] = useState<string>("");
+  const handleOnConfirmBtnclick = () => {
+    if (localStorage.getItem("wallet")) {
+      localStorage.setItem(
+        "wallet",
+        `${(
+          parseFloat(localStorage.getItem("wallet") || "0") + parseFloat(money)
+        ).toFixed(3)}`
+      );
+    }
+  };
+
+  const handleOnMoneyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMoney(e.target.value);
+  };
+  return (
+    <>
+      <ExitToMainMenu />
+
+      <div className="cardInfoCont">
+        <form action="">
+          <h1 className="log">Payment info</h1>
+
+          <div>
+            <label htmlFor="name">Full name</label>
+            <input className="name" type="text" placeholder="Sasha X" />
+          </div>
+
+          <div>
+            <label htmlFor="cardNumb">Credit card number</label>
+            <input
+              className="cardNumb"
+              type="text"
+              placeholder="1234 1234 1234 1234"
+            />
+          </div>
+
+          <div className="dateCont">
+            <div>
+              <label htmlFor="dateOfend">Exp date</label>
+              <input className="dateOfend" type="text" placeholder="MM/YY" />
+            </div>
+
+            <div>
+              <label htmlFor="cvv">CVV</label>{" "}
+              <input className="cvv" type="password" placeholder="***" />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="code">Аmount of money</label>
+            <input
+              value={money}
+              className="code"
+              type="number"
+              placeholder="100$"
+              onChange={handleOnMoneyChange}
+            />
+          </div>
+
+          <div className="payBtn" onClick={handleOnConfirmBtnclick}>
+            Confirm Payment
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
