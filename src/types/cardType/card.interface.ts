c67cd6b20147fdcc02418c104d68269d8bee7039
Alexund <ds.alex@tut.by>
2025-04-17 17:22:12 +0300

    fixis

--- src/types/cardType/card.interface.ts
+++ src/types/cardType/card.interface.ts
@@ -2,10 +2,7 @@ import { JSX } from "react/jsx-runtime";
 
 export interface crptItm {
   id: string;
-  tsupply: string;
   name: string;
-  nameid: string;
   percent_change_1h: string;
   price_usd: string;
-  rank: number;
 }
